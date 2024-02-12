8a4885be1ba158826758c760c30cba525f31b6f6
jasper villarosa <jvillarosa@up.edu.ph>
2024-02-12 10:21:36 +0800

    removed transitions

--- src/main.tsx
+++ src/main.tsx
@@ -1,68 +1,22 @@
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import "./index.css";
-import Calculator from "./pages/Calculator";
-import JSON from "./pages/JSON";
-import MyHobby from "./pages/MyHobby";
-import React from "react";
-import { motion, AnimatePresence } from "framer-motion";
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Route, Routes} from 'react-router-dom'
+import Home from './pages/Home'
+import './index.css'
+import Calculator from './pages/Calculator'
+import JSON from './pages/JSON'
+import MyHobby from './pages/MyHobby'
+import React from 'react'
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <AnimatePresence mode="wait">
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <motion.div
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                exit={{ opacity: 0 }}
-              >
-                <Home />
-              </motion.div>
-            }
-          />
-          <Route
-            path="/calculator"
-            element={
-              <motion.div
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                exit={{ opacity: 0 }}
-              >
-                <Calculator />
-              </motion.div>
-            }
-          />
-          <Route
-            path="/JSON"
-            element={
-              <motion.div
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                exit={{ opacity: 0 }}
-              >
-                <JSON />
-              </motion.div>
-            }
-          />
-          <Route
-            path="/myHobby"
-            element={
-              <motion.div
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                exit={{ opacity: 0 }}
-              >
-                <MyHobby />
-              </motion.div>
-            }
-          />
-        </Routes>
-      </AnimatePresence>
+      <Routes>
+         <Route path='/' element={<Home/>}></Route>
+         <Route path='/calculator' element={<Calculator/>}></Route>
+         <Route path='/JSON' element={<JSON/>}></Route>
+         <Route path='/myHobby' element={<MyHobby/>}></Route>
+      </Routes>
     </BrowserRouter>
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
